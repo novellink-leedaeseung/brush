f7dca20fe176ec1b0c9b21a4be65550c1edaf925
lee <eotmd1016@gmail.com>
2025-09-09 17:30:58 +0900

    키오스크 토큰 가져오기 및 코드 리팩토링

--- src/main.ts
+++ src/main.ts
@@ -2,30 +2,36 @@ import { getKioskAuth } from "./api/ApiAxios";
 
 type StatusType = "info" | "success" | "error";
 
-const statusEl = document.getElementById("status") as HTMLDivElement | null;
-const outputEl = document.getElementById("output") as HTMLPreElement | null;
-const btnFetch = document.getElementById("btn-fetch") as HTMLButtonElement | null;
-const userId = document.getElementById("user-id") as HTMLInputElement | null;
+const kioskAuth = {
+    "status" : document.getElementById("kiosk-auth-status") as HTMLDivElement | null,
+    "output" : document.getElementById("kiosk-auth-output") as HTMLPreElement | null,
+    "btn-fetch": document.getElementById("kiosk-auth-btn-fetch") as HTMLButtonElement | null,
+    "user-id": document.getElementById("kiosk-auth-user-id") as HTMLInputElement | null,
+};
 
 function setStatus(type: StatusType, text: string) {
-  if (!statusEl) return;
-  statusEl.dataset.type = type;
-  statusEl.textContent = text;
+  if (!kioskAuth.status) return;
+  kioskAuth.status.dataset.type = type;
+  kioskAuth.status.textContent = text;
 }
 
 
 async function loadUsers() {
   setStatus("info", "사용자 목록 불러오는 중…");
   try {
-    const users = await getKioskAuth(userId?.value ?? "");
-    setStatus("success", `불러오기 완료 (${users.resultData.token}명)`);
+    const users = await getKioskAuth(kioskAuth["user-id"]?.value ?? "");
+    setStatus("success", `불러오기 완료`);
+    if (kioskAuth["output"]) {
+      kioskAuth["output"].textContent = `${users.resultCode} - Token: ${users.resultData.token}`;
+    }
+
   } catch (err: any) {
     console.error(err);
     setStatus("error", "불러오기에 실패했습니다.");
-    if (outputEl) outputEl.textContent = "오류가 발생했습니다. 콘솔을 확인하세요.";
+    if (kioskAuth.output) kioskAuth.output.textContent = "오류가 발생했습니다. 콘솔을 확인하세요.";
   }
 }
 
-btnFetch?.addEventListener("click", () => {
+kioskAuth["btn-fetch"]?.addEventListener("click", () => {
   void loadUsers();
 });
