b6a7c2fe783c21fdfc4a32d64f0510583f458d0d
lee <eotmd1016@gmail.com>
2025-09-11 10:11:43 +0900

    https 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-    proxy: {
-      "/api": {
-        target: "https://novel.rosq.co.kr:8488/",
-        changeOrigin: true,
-        secure: true, // 자체 서명 인증서 등으로 문제가 있으면 false로
-        // rewrite: (path) => path.replace(/^\/api/, ""),
-      },
+    plugins: [basicSsl()],
+    server: {
+        https: true,
+        host: true,     // 네트워크 접근 허용(모바일 기기 테스트 등)
+        proxy: {
+            "/api": {
+                target: "https://novel.rosq.co.kr:8488/",
+                changeOrigin: true,
+                secure: true, // 자체 서명 인증서 등으로 문제가 있으면 false로
+                // rewrite: (path) => path.replace(/^\/api/, ""),
+            },
+        },
     },
-  },
 });
