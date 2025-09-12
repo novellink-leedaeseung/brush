20278d646cd81e62d9f1d3a96f5db1f511274705
lee <eotmd1016@gmail.com>
2025-09-12 13:26:49 +0900

    키패드에 전화번호를 입력 했을 때 API 통신가능하게 하였음

--- src/user-find/userFindApp.ts
+++ src/user-find/userFindApp.ts
@@ -11,16 +11,27 @@ confirmButton.addEventListener('click', () => {
 // 1. 키오스크 토큰 가져오기
 // 2. inputField에 전화번호로 조회하기
 export function findUser(inputField: string) {
+    localStorage.clear();
+    // 핸드폰, 회원 아이디 구분
+    let type = "PHONE";
+    if (inputField.length !== 11 && inputField.indexOf("010", 0) !== 0) {
+        type = "ID";
+    }
+
     // 키오스크 인증 후 사용자 인증
     getKioskAuth("MTA001")
         .then(res => {
             const kioskToken = res.resultData.token;
             // kioskToken을 받은 후 사용자 인증 실행
-            return getAuthUser(inputField, "PHONE", kioskToken);
+            return getAuthUser(inputField, type, kioskToken);
         })
         .then(res => {
             console.log('사용자 인증 결과:', res);
             // 여기서 인증 성공 후 처리
+            localStorage.setItem("name", res.resultData.username);
+            localStorage.setItem("gender", res.resultData.gender);
+            localStorage.setItem("phone", res.resultData.phonenumber);
+            window.location.href = "/kiosk/user-confirm.html";
         })
         .catch(error => {
             console.error('에러 발생:', error);
