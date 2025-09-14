d491aa6f9525a074075920f3bb962696a75827d1
lee <eotmd1016@gmail.com>
2025-09-14 12:56:52 +0900

    양치 인증 완료

--- src/camara/confirm.ts
+++ src/camara/confirm.ts
@@ -5,10 +5,11 @@ function main() {
     const img = document.getElementById('capturedImg') as HTMLImageElement | null;
     const dataUrl = sessionStorage.getItem(CAPTURED_PHOTO_KEY);
 
+
     if (!img || !dataUrl) {
         // 저장된 이미지가 없으면 촬영 페이지로 되돌림
-        location.replace('/kiosk/camara.html');
-        return;
+        // location.replace('/kiosk/camara.html');
+        // return;
     }
 
     img.src = dataUrl;
@@ -18,17 +19,28 @@ function main() {
 
     retakeBtn?.addEventListener('click', () => {
         sessionStorage.removeItem(CAPTURED_PHOTO_KEY);
-        location.href = '/kiosk/camara.html';
+        // location.href = '/kiosk/camara.html';
     });
 
     downloadBtn?.addEventListener('click', () => {
+        const modal = document.getElementById('confirmationModal');
+
+
+        // 모달 표시
+        modal.classList.add('show');
+
+        // 3초 후 자동으로 모달 닫기
+        setTimeout(() => {
+            modal.classList.remove('show');
+        }, 3000);
+
         let phoneNumber = localStorage.getItem("phone");
         let name = localStorage.getItem("name");
         // 파일명
         let today = new Date();
 
         let dd = String(today.getDate()).padStart(2, '0'); // 시 분
-        let mm = String(today.getMonth()+1).padStart(2, '0'); // 월
+        let mm = String(today.getMonth() + 1).padStart(2, '0'); // 월
         let yyyy = today.getFullYear(); // 년도
 
         let fileName = yyyy + '-' + mm + '-' + dd + '-' + phoneNumber + '-' + name;
