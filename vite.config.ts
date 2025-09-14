145b6b82386da64801359ba11f385cc111498c18
lee <eotmd1016@gmail.com>
2025-09-14 19:08:42 +0900

    API 프록시 설정

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,14 @@ export default defineConfig({
   server: {
     port: 5173,
     host: true,
-    https: true
+    https: true,
+    proxy: {
+            "/api": {
+                target: "https://novel.rosq.co.kr:8488/",
+                changeOrigin: true,
+                secure: true, // 자체 서명 인증서 등으로 문제가 있으면 false로
+                // rewrite: (path) => path.replace(/^\/api/, ""),
+            },
+        },
   }
 })
